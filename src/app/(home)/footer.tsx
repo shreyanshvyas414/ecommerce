708b8423c40e392cb0f9e197d6e3d4bc07114b95
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2025-04-16 20:25:51 +0530

    :sparkles: Home layout and navigation setup

---
+++ src/app/(home)/footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="flex border-t justify-between font-medium p-6">
+        <div className="flex items-center gap-2">
+          <p>Footer, Inc.</p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
